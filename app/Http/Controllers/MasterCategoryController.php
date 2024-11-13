c89d744bc32484790d1d809bcc8bf1e29356fc83
Kay Zin Khaing <kayzinkhaing1331@gmail.com>
2024-11-13 15:16:02 +0630

    first

---
+++ app/Http/Controllers/MasterCategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class MasterCategoryController extends Controller
+{
+    public function storecat(Request $request){
+        $validate_data= $request->validate([
+            'category_name'=>'unique:categories|max:100|min:10',
+        ]);
+        Category::create($validate_data);
+        return redirect()->back();
+    }
+}
---
+++ resources/views/admin/sub_category/create.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.layout')
+@section('admin_layout')
+    <h3>Create Sub Category Page</h3>
+
+@endsection
